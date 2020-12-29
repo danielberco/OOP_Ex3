583840bcbdd1e2f58fe5047a17ad0f8c465bfdf7
MtheEPIC <mtheepic1423@protonmail.com>
2020-12-29 05:48:42 +0200

    copied all func names to implement

--- pythonProject/src/GraphAlgo.py
+++ pythonProject/src/GraphAlgo.py
@@ -1,26 +1,22 @@
 from typing import List
-from src import GraphInterface
+from src.GraphAlgoInterface import GraphAlgoInterface
 
 
-class GraphAlgo(GraphInterface.GraphInteface):
-
-    def get_graph(self) -> GraphInterface:
-        raise NotImplementedError
-
+class GraphAlgo(GraphAlgoInterface):
     def load_from_json(self, file_name: str) -> bool:
-        raise NotImplementedError
+        pass
 
     def save_to_json(self, file_name: str) -> bool:
-        raise NotImplementedError
+        pass
 
     def shortest_path(self, id1: int, id2: int) -> (float, list):
-        raise NotImplementedError
+        pass
 
     def connected_component(self, id1: int) -> list:
-        raise NotImplementedError
+        pass
 
     def connected_components(self) -> List[list]:
-        raise NotImplementedError
+        pass
 
     def plot_graph(self) -> None:
-        raise NotImplementedError
+        pass
