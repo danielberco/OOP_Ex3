6756cb92dc6dba7c943ecf7072a11f5d53c385a0
MtheEPIC <mtheepic1423@protonmail.com>
2021-01-02 22:07:12 +0200

    fixed json type errors

--- pythonProject/src/GraphAlgo.py
+++ pythonProject/src/GraphAlgo.py
@@ -3,6 +3,7 @@ import json
 from json import JSONEncoder
 from src.GraphAlgoInterface import GraphAlgoInterface
 from src.DiGraph import DiGraph
+from src.NodeDS import NodeDS
 
 
 class MyEncoder(JSONEncoder):
@@ -22,7 +23,21 @@ class GraphAlgo(GraphAlgoInterface):
         except FileNotFoundError:
             return False
         self.g.__dict__.update(self.json)
+        self.change_key_to_int("node_obj", create_obj=True)
+        self.change_key_to_int("nodes")
         return True
+
+    def change_key_to_int(self, dict_name, create_obj=False) -> None:
+        sub_dict = self.g.__dict__[dict_name]
+        tmp_dict = {}
+        for k in sub_dict.keys():
+            if create_obj:
+                tmp_dict[ord(k) - 48] = NodeDS(self.g.__dict__[dict_name][k])
+            else:
+                tmp_dict[ord(k) - 48] = sub_dict[k]
+        del sub_dict
+        self.g.__dict__[dict_name] = {}
+        self.g.__dict__[dict_name].update(tmp_dict)
 
     def save_to_json(self, file_name: str) -> bool:
         self.json = self.g.__dict__
