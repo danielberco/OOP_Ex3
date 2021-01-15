ba28994a0c6578071c184aa51cb75a34f019b568
MtheEPIC <mtheepic1423@protonmail.com>
2021-01-15 19:09:46 +0200

    imroved test for shortest path

--- pythonProject/src/test_GraphAlgo.py
+++ pythonProject/src/test_GraphAlgo.py
@@ -32,28 +32,50 @@ class TestGraphAlgo(TestCase):
     def test_shortest_path(self):
         g = DiGraph()
         ga = GraphAlgo(g)
-
         w = 5
         r = 10
-
-        g.add_edge(0, 9, w * r + 1)
         length, path = ga.shortest_path(0, 9)
-        self.assertEqual((r - 1) * w, length)
+        self.assertEqual(float('inf'), length)
+
+        for i in range(0, r):
+            g.add_node(i)
+        l = w * r + 1
+        g.add_edge(0, 9, l)
+        length, path = ga.shortest_path(0, 9)
+        self.assertEqual(l, length)
 
         for i in range(0, r):
             g.add_node(i)
         for i in range(0, r - 1):
             g.add_edge(i, i + 1, w)
 
-
         length, path = ga.shortest_path(0, 9)
         self.assertEqual((r-1)*w, length)
 
         g.add_edge(0, 8, 1)
+        length, path = ga.shortest_path(0, 9)
         self.assertEqual(w+1, length)
 
     def test_connected_component(self):
-        return NotImplemented
+        """
+        run by func2 and check the connected_component by each node in the graph.
+        This functions tests the connected_component function for the class DiGraph
+        """
+        self.func2()
+        my_list = [[0, 1, 2, 3, 4, 5, 6], [8], [9]]
+        i = 0
+
+        for x in self.list_graph[0].get_all_v().values():
+            str = "i = {} , {} -> {} == {}".format(i, x.get_key(), self.list_algo[0].connected_component(x.get_key()),
+                                                   my_list[0])
+            if i < 7:
+                self.assertEqual(my_list[0], self.list_algo[0].connected_component(x.get_key()))
+            if i == 7:
+                self.assertEqual(my_list[1], self.list_algo[0].connected_component(x.get_key()))
+
+            if i == 8:
+                self.assertEqual(my_list[2], self.list_algo[0].connected_component(x.get_key()))
+            i += 1
 
     def test_connected_components(self):
         return NotImplemented
@@ -76,4 +98,29 @@ class TestGraphAlgo(TestCase):
         ga = GraphAlgo(g)
 
         ga.plot_graph()
+
+    def func2(self):
+        """
+        create specific graph for some tests.
+        with 9 vertex.
+        """
+        graph = DiGraph()
+        for x in range(7):
+            graph.add_node(x)
+        for x in graph.get_all_v().values(): #.get_all_values():
+            graph.add_edge(x.get_key(), x.get_key() + 1, x.get_key() + 1)
+            graph.add_edge(x.get_key(), x.get_key() - 1, x.get_key() + 1)
+
+        graph.add_node(8)
+        graph.add_node(9)
+        algo = GraphAlgo(graph)
+        algo.plot_graph()
+        str1 = "{}".format(algo.get_graph())
+        # str2 = "{}".format(graph.toStringInAndOut())
+        print(str1)
+        # print(str2)
+        self.list_algo = []
+        self.list_graph = []
+        self.list_algo.append(algo)
+        self.list_graph.append(algo.get_graph())
 
